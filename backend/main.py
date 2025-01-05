da74ef366811ee05ba23589e0f3c09e3627d598e
CosminPanciuc <ilie-cosmin.panciuc@student.tuiasi.ro>
2025-01-05 18:11:10 +0200

    implement pso

---
+++ backend/main.py
@@ -0,0 +1,74 @@
+from datetime import datetime, timedelta
+
+import numpy as np
+import yfinance as yf
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from backend.pso import PSO
+
+
+def fetch_stock_data(symbol, start_date, end_date):
+    stock_data = yf.download(symbol, start=start_date, end=end_date, progress=False)
+
+    prices = stock_data["Close"]
+
+    return prices
+
+
+def calculate_return(prices):
+    start_price = prices.iloc[0]
+    end_price = prices.iloc[-1]
+
+    stock_return = (end_price - start_price) / start_price
+
+    return stock_return
+
+
+def calculate_covariance(stock_prices):
+    daily_returns = stock_prices.pct_change().dropna() * 10
+
+    covariance_matrix = daily_returns.cov()
+
+    return np.array(covariance_matrix)
+
+
+app = FastAPI()
+
+
+class PortfolioInput(BaseModel):
+    symbols: list
+    risk: float | None = 100
+    num_particles: int | None = 50
+    iter: int | None = 100
+
+
+@app.post("/optimize")
+def optimize_portfolio(data: PortfolioInput):
+    start_date = (datetime.now() - timedelta(days=365 * 5)).strftime("%Y-%m-%d")
+    end_date = datetime.now().strftime("%Y-%m-%d")
+
+    stock_prices = fetch_stock_data(data.symbols, start_date, end_date)
+    print(stock_prices)
+
+    returns = np.array([calculate_return(stock_prices[symbol]) for symbol in stock_prices])
+
+    covariance = calculate_covariance(stock_prices)
+
+    pso = PSO(
+        num_particles=data.num_particles,
+        num_assets=len(data.symbols),
+        returns=returns,
+        covariance=covariance,
+        risk=data.risk,
+        iter=data.iter,
+    )
+
+    # Perform optimization
+    optimal_allocation = pso.optimize()
+
+    # Return the optimal portfolio allocation
+    return {
+        "symbols": data.symbols,
+        "allocation": optimal_allocation.tolist(),
+    }
---
+++ backend/pso.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+
+def portfolio_objective(weights, returns, covariance, risk=100):
+    portfolio_return = np.dot(weights, returns)
+    portfolio_risk = np.dot(weights.T, np.dot(covariance, weights))
+
+    penalty = 0
+    for weight in weights:
+        if weight > 0.5:
+            penalty += 1000000
+
+    return -(portfolio_return - risk * portfolio_risk) + penalty
+
+
+class PSO:
+    def __init__(self, num_particles, num_assets, returns, covariance, risk=1, iter=100):
+        self.num_particles = num_particles
+        self.num_assets = num_assets
+        self.returns = returns
+        self.covariance = covariance
+        self.risk = risk
+        self.iter = iter
+        self.inertia = 0.5
+        self.cognitive = 2.0
+        self.social = 2.0
+
+        self.positions = np.random.dirichlet(np.ones(num_assets), size=num_particles)
+        self.velocities = np.random.uniform(-1, 1, (num_particles, num_assets))
+
+        self.personal_best_positions = self.positions.copy()
+        self.personal_best_scores = np.array([self.evaluate(p) for p in self.positions])
+        self.global_best_position = self.personal_best_positions[
+            np.argmin(self.personal_best_scores)
+        ]
+
+    def evaluate(self, weights):
+        return portfolio_objective(weights, self.returns, self.covariance, self.risk)
+
+    def optimize(self):
+        for iteration in range(self.iter):
+            for i, position in enumerate(self.positions):
+                score = self.evaluate(self.positions[i])
+
+                r1 = np.random.rand()
+                r2 = np.random.rand()
+
+                if score < self.personal_best_scores[i]:
+                    self.personal_best_scores[i] = score
+                    self.personal_best_positions[i] = self.positions[i]
+
+                self.velocities[i] = (
+                    self.inertia * self.velocities[i]
+                    + self.cognitive * r1 * (self.personal_best_positions[i] - position)
+                    + self.social * r2 * (self.global_best_position - position)
+                )
+
+                self.positions[i] += self.velocities[i]
+                self.positions[i] = np.clip(self.positions[i], 0, 0.5)
+                self.positions[i] /= np.sum(self.positions[i])
+
+            self.global_best_position = self.personal_best_positions[
+                np.argmin(self.personal_best_scores)
+            ]
+
+        return self.global_best_position
